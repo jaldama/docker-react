fd7884497204070b5f07e1a41107a21fa6961d75
jaldama <isayrhymes@hotmail.com>
2019-03-16 20:10:55 -0700

    change app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            smelly
+            This is a change and new branch
           </p>
           <a
             className="App-link"
